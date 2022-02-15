26e4e73d28127242d63294c55dbbd3889436024c
Tareq Mansour <46700175+trqmnsr@users.noreply.github.com>
2022-02-15 12:12:12 -0800

    Get it going

--- src/gui/MultiMeterGUI.py
+++ src/gui/MultiMeterGUI.py
@@ -1,32 +1,46 @@
+import random
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.uix.button import Button
+from kivy.uix.recycleview import RecycleView
+from kivy.clock import Clock
+from mygraph import GraphLayout
 
+ModeButtonsOptions = ['V~', 'V=', 'A', 'Ω', 'C/F', 'Light', '+']
 
 
 class ModeButton(Widget):
+    counter = 0
 
-    def __init__(self, text,  **kwargs):
+    def __init__(self, text, **kwargs):
         super(ModeButton, self).__init__(**kwargs)
         button = Button()
-        button.width = self.width
-        button.height = self.height
+        button.width = 300
+        button.height = 100
         button.pos = self.pos
         button.color = "red"
-        button.text = kwargs.get('text')
+        button.text = text
         self.add_widget(button)
-
 
     def on_touch_down(self, touch):
         # do action when pressed down
-        print("pressed button")
+        print("pressed button {}".format(self.counter))
+        self.counter += 1
 
 
+class ModeSelectList(RecycleView):
 
+    def __init__(self, **kwargs):
+        super(ModeSelectList, self).__init__(**kwargs)
+        self.size_hint_max_x = 85
+        self.data = [
+            {'text': str(x), 'width': 75, 'height': 75}
+            for x in ModeButtonsOptions
+        ]
 
 
 class MainLayout(BoxLayout):
@@ -34,30 +48,36 @@ class MainLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(MainLayout, self).__init__(**kwargs)
         self.orientation = 'horizontal'
-        menu = BoxLayout(orientation='vertical')
-        menu.width = 100
-        btn1 = Button(text='Hello', width=100)
-        btn2 = Button(text='World', width=100)
-        menu.add_widget(btn1)
-        menu.add_widget(btn2)
-        menu.spacing = 20
-        menu.padding = 20
-        self.add_widget(menu, 0)
-        btn3 = Button(text='All alone')
-        self.add_widget(btn3)
+        menu = ModeSelectList()
+        self.add_widget(menu)
+        self.graph = GraphLayout()
+        self.add_widget(self.graph)
 
 
-        
+class MainGrid(GridLayout):
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 2
+        menu = ModeSelectList()
+        self.add_widget(menu, index=1)
+        self.graph = GraphLayout()
+        self.add_widget(self.graph, index=0)
+        Clock.schedule_interval(self.fakeData, 1 / 10)
 
+    testpoint = 120
+
+    def fakeData(self, *args):
+        self.testpoint += random.choice([-1, 1]) * random.random()
+        self.graph.addpoint(self.testpoint, args[0])
 
 
 class MultiMeterApp(App):
     def build(self):
         self.title = "UCDAVIS Bluetooth Multimeter"
-        Window.minimum_height = 600
-        Window.minimum_width = 800
-        app = MainLayout()
+        Window.minimum_height = 400
+        Window.minimum_width = 600
+        app = MainGrid()
         return app
 
 
---
+++ src/gui/mygraph.py
@@ -0,0 +1,249 @@
+from kivy.core.window import Window
+from kivy.input.providers.mouse import MouseMotionEvent
+from kivy.uix.popup import Popup
+
+from graph import Graph, MeshLinePlot, VBar, Point, PointPlot
+from kivy.utils import get_color_from_hex as rgb
+from kivy.clock import Clock
+import itertools
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.dropdown import DropDown
+
+
+class GraphProfile:
+        title = 'Volts in AC'
+        xmax = 10.0
+        xmin = 0
+        ymin = 0
+        ymax = 160
+        xlabel = 'Time (s)'
+        ylabel = 'Volts (Vrms)'
+
+
+class GraphWidget(Graph):
+    def __init__(self, graphProfile: GraphProfile ,**kwargs):
+        super().__init__(**kwargs)
+        colors = itertools.cycle([
+            rgb('7dac9f'), rgb('dc7062'), rgb('66a8d4'), rgb('e5b060')])
+        self.profile = graphProfile
+        self.xlabel = graphProfile.xlabel
+        self.ylabel = graphProfile.ylabel
+        self.x_ticks_minor = 1
+        self.x_ticks_major = 1
+        self.y_ticks_major = 20
+        self.y_grid_label = True
+        self.x_grid_label = True
+        self.padding = 0
+        self.xlog = False
+        self.ylog = False
+        self.x_grid = True
+        self.y_grid = True
+        self.xmin = graphProfile.xmin
+        self.xmax = graphProfile.xmax
+        self.ymin = graphProfile.ymin
+        self.ymax = graphProfile.ymax
+        self.background_color = rgb('ffffffff')
+        self.label_options = {'color': rgb('444444'), 'bold': True}
+        self.border_color = rgb('f8f8f2')
+        self.tick_color = rgb('808080')
+        self.border_color = rgb('808080')
+        self.queue = myQueue(100)
+        self.point_label = Label(font_size='9sp')
+        self.point_label.color = rgb('000000')
+        self.add_widget(self.point_label)
+        self.plot = MeshLinePlot(color=rgb('ff0000'))
+        #self.view_size = 10
+        self.add_plot(self.plot)
+        self.vbar = VBar()
+        self.vbar.color = rgb('5e34eb')
+        self.add_plot(self.vbar)
+        self.plot_points = PointPlot(color=rgb('000000'))
+        self.plot_points.point_size = 10
+        self.add_plot(self.plot_points)
+        self.currentTime = 0
+        self.evenTrigger = Clock.create_trigger(self.update_points, interval=True, timeout=1/10)
+        self.bind(on_touch_up=self._touch_up)
+
+
+    def _touch_up(self, *touch):
+        self.x_start = self.pos[0] + self.view_pos[0]
+        self.x_end = self.x_start + self.view_size[0]
+        self.y_start = self.pos[1] + self.view_pos[1]
+        self.y_end = self.y_start + self.view_size[1]
+        if self.x_start < touch[1].x < self.x_end and self.y_start < touch[1].y < self.y_end:
+            x, y = self.window_position_to_graph_value(*touch[1].pos)
+            if 0 != len(self.plot.points):
+                # https://www.geeksforgeeks.org/python-find-closest-number-to-k-in-given-list/
+                point_ref = self.plot.points[min(range(len(self.plot.points)),
+                                                 key=lambda i: abs(self.plot.points[i][0]-x))]
+                self.vbar.points = [point_ref[0]]
+                self.point_label.text = '({0:.3f},{1:.3f})'.format(x, y)
+                edge = Window.width - self.graph_pos_to_window_pos(*point_ref)[0] - self.point_label.width
+                if edge < 0:
+                    self.point_label.pos = Window.width - self.point_label.width - 10, self.graph_pos_to_window_pos(*point_ref)[1]
+                else:
+                    self.point_label.pos = self.graph_pos_to_window_pos(*point_ref)
+                self.plot_points.points.append(point_ref)
+                print(f'X:{x} Y:{y}')
+
+    def window_position_to_graph_value(self, x, y):
+        x_out = self.xmin + (x - self.x_start) * (self.xmax - self.xmin) / self.view_size[0]
+        y_out = self.ymin + (y - self.y_start) * (self.ymax - self.ymin) / self.view_size[1]
+        return x_out, y_out
+
+    def graph_pos_to_window_pos(self, x, y):
+        x_out = self.pos[0] + self.view_pos[0] + (x - self.xmin) * (self.view_size[0]) / (self.xmax - self.xmin)
+        y_out = self.pos[1] + self.view_pos[1] + (y - self.ymin) * (self.view_size[1]) / (self.ymax - self.ymin)
+        return x_out, y_out
+
+    def startClock(self):
+        self.evenTrigger()
+
+    def pauseClock(self):
+        Clock.unschedule(self.evenTrigger)
+
+    def reset(self):
+        self.currentTime = 0
+        self.xmax = 10
+        self.xmin = 0
+        self.queue.clear()
+        self.plot.points = self.queue.get()
+
+    def add_point(self, value, timeDif):
+        time = self.currentTime + timeDif
+        #print(f'V: {value} , Time:{time}')
+        if time > self.xmax:
+            self.xmax += 5
+            self.xmin += 5
+        if len(self.plot_points.points):
+            self.point_label.pos = self.graph_pos_to_window_pos(*self.plot_points.points[0])
+        self.currentTime = time
+        self.queue.addToeQueue(value, time)
+
+
+    def update_points(self, *args):
+        self.plot.points = self.queue.get()
+
+
+class myQueue:
+    def __init__(self, size):
+        self.queue = []
+        self.size = size
+
+    def addToeQueue(self, newValue, time):
+        if len(self.queue) > self.size:
+            self.queue.pop()
+        self.queue.insert(0, (time, newValue))
+
+    def get(self):
+        return self.queue
+
+    def clear(self):
+        self.queue.clear()
+
+    def __len__(self):
+        return len(self.queue)
+
+    def __index__(self):
+        return self.queue
+
+
+class SaveButtonWithDropdown(Button):
+
+    def __init__(self, assigned_graph, **kwargs):
+        super().__init__(**kwargs)
+        #self.size_hint=(None, None)
+        self.dropdown = DropDown()
+        btnSaveImage = Button(text='PNG', size_hint_y=None, height=50)
+        btnSaveData = Button(text='Data', size_hint_y=None, height=50)
+        btnSaveImage.bind(on_release=self.selectSaveImage)
+        btnSaveData.bind(on_release=self.selectSaveData)
+        self.assignedGraph: GraphWidget = assigned_graph
+        self.dropdown.add_widget(btnSaveImage)
+        self.dropdown.add_widget(btnSaveData)
+        self.dropdown.opacity = 0
+        self.dropdown.auto_dismiss = True
+        self.add_widget(self.dropdown)
+
+    def on_release(self, *args):
+        self.dropdown.open(self)
+        self.dropdown.opacity = 100
+
+    def selectSaveImage(self, *args):
+        self.assignedGraph.export_to_png("image.png")
+        print('saveimage')
+        pass
+
+    def selectSaveData(self, *args):
+        data_to_save = {
+            'xmin': self.assignedGraph.xmin,
+            'xmax': self.assignedGraph.xmax,
+            'ymin': self.assignedGraph.ymin,
+            'ymax': self.assignedGraph.ymax,
+            'points': self.assignedGraph.plot.points,
+        }
+        with open('test.txt', 'w') as file:
+            file.write(data_to_save.__str__())
+            popup = Popup(title='File Saved',
+                          content=Label(text='Hello world'),
+                          size_hint=(None, None), size=(200, 200))
+
+            btnOk = Button(text="OK")
+            popup.content = btnOk
+            popup.open()
+        print('savedata')
+        pass
+
+
+class GraphLayout(GridLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rows = 2
+        # Graph setup
+        self.graph = GraphWidget(GraphProfile())
+        # Button Setup
+        buttonRow = BoxLayout()
+        buttonRow.orientation = "horizontal"
+        buttonRow.spacing = 100
+        buttonRow.size_hint_max_y = 100
+        buttonRow.padding = [100, 0]
+        config = Button(text='Config')
+        config.bind(on_press=self.configPress)
+        recordButton = Button(text='Record')
+        recordButton.bind(on_press=self.recordPress)
+        resetButton = Button(text='Reset')
+        resetButton.bind(on_press=self.resetPress)
+        saveDropdown = SaveButtonWithDropdown(text='Save', assigned_graph=self.graph)
+        buttonRow.add_widget(recordButton)
+        buttonRow.add_widget(resetButton)
+        buttonRow.add_widget(saveDropdown)
+        buttonRow.add_widget(config)
+
+        self.add_widget(buttonRow)
+        self.add_widget(self.graph)
+        self.state_record = True
+
+
+    def recordPress(self, *args):
+        if self.state_record:
+            self.state_record = False
+            args[0].text = 'START'
+            self.graph.pauseClock()
+        else:
+            self.state_record = True
+            args[0].text = 'PAUSE'
+            self.graph.startClock()
+
+    def configPress(self, *args):
+        print('config done')
+
+    def resetPress(self, *args):
+        self.graph.reset()
+
+    def addpoint(self, value, time):
+        if self.state_record:
+            self.graph.add_point(value, time)

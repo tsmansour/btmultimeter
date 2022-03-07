9e40df57e680a3b042d90c94fe3569ff2344fa37
Tareq Mansour <46700175+trqmnsr@users.noreply.github.com>
2022-03-06 18:33:03 -0800

    working bluetoothDecoder.py. Will return value, range and type

--- src/bluetoothDecoder.py
+++ src/bluetoothDecoder.py
@@ -10,12 +10,12 @@ MODES = {
 
 # Add dividers and max/min when available here
 ATTENUATION = {
-	0: {'Voltmeter': (0.00001, 0.4, -0.4), 'Ohmmeter': (.001, 400, 0)},
-	1: {'Voltmeter': (0.0001, 4.0, -4.0), 'Ohmmeter': (0.1, 4000, 0)},
-	2: {'Voltmeter': (0.001, 40, -40), 'Ohmmeter': (1, 40000, 0)},
-	3: {'Voltmeter': (0.01, 400, -400), 'Ohmmeter': (10, 400000, 0)},
-	4: {'Voltmeter': (0.1, 4000, -4000), 'Ohmmeter': (100, 4000000, 0)},
-	5: {'Voltmeter': (1, 40000, -40000), 'Ohmmeter': (1000, 40000000, 0)},
+	0: {'Voltmeter': (0.00001,  0.4,    -0.4),      'Ohmmeter': (.001,  400,        0)},
+	1: {'Voltmeter': (0.0001,   4.0,    -4.0),      'Ohmmeter': (0.1,   4000,       0)},
+	2: {'Voltmeter': (0.001,    40,     -40),       'Ohmmeter': (1,     40000,      0)},
+	3: {'Voltmeter': (0.01,     400,    -400),      'Ohmmeter': (10,    400000,     0)},
+	4: {'Voltmeter': (0.1,      4000,   -4000),     'Ohmmeter': (100,   4000000,    0)},
+	5: {'Voltmeter': (1,        40000,  -40000),    'Ohmmeter': (1000,  40000000,   0)},
 }
 
 DEFAULT_ATTENUATION = (1, 40000, -40000)
@@ -54,11 +54,12 @@ class BluetoothDecoder:
 	def getNextPoint(self):
 		if self.storedData.empty():
 			return None
-		return self.storedData.get()
+		return self.storedData.get_nowait()
 
 	def _updateAll(self):
 		mode = MODES[self.bufferValues[0]]
-		attenuation = ATTENUATION.get(self.bufferValues[6], DEFAULT_ATTENUATION).get(mode, DEFAULT_ATTENUATION)
+		attenuation = ATTENUATION.get(
+			self.bufferValues[6], DEFAULT_ATTENUATION).get(mode, DEFAULT_ATTENUATION)
 		value = self._getValue()
 		value = value * attenuation[0]
 		self.storedData.put({
@@ -67,3 +68,28 @@ class BluetoothDecoder:
 			"min_y": attenuation[2],
 			"value": value,
 		})
+
+
+import random
+
+def fakeBluetooth():
+	count = 0
+	while True:
+		print(f'{count}')
+		x = bytearray()
+		if count == 0:
+			x.append(2)
+		elif count == 6:
+			x.append((count<<4) + 2)
+		else:
+			testpoint = random.randint(0, 4)
+			testpoint = (count << 4) + testpoint
+			x.append(testpoint)
+		if count == 6:
+			count = 0
+		else:
+			count += 1
+		yield x
+
+# x = fakeBluetooth()
+# next(x)
